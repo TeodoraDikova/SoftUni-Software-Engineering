4ddda499337fd5e30e4d320c5e26671a11e4cc0c
Teodora Dikova <145490538+TeodoraDikova@users.noreply.github.com>
2023-10-07 20:37:41 +0300

    Add files via upload
    02-DaysOfWeek Uploaded

---
+++ JavaScript-Programming-Fundamentals/02-Arrays/Lab/02-DaysOfWeek.js
@@ -0,0 +1,19 @@
+function daysOfWeek(index){
+    let days = [
+     'Monday',
+     'Tuesday',
+     'Wednesday',
+     'Thursday',
+     'Friday',
+     'Saturday',
+     'Sunday'
+    ];
+    
+    let result = days[index - 1]
+
+    if (result != undefined){
+        console.log(result);
+    }else{
+        console.log('Invalid day!');
+    }  
+}
