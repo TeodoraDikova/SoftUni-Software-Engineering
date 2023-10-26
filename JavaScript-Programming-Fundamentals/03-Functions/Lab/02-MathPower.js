f3fef4fa1a3d38ecf9ed36503f5d76cedc58c214
Teodora Dikova <145490538+TeodoraDikova@users.noreply.github.com>
2023-10-26 19:52:13 +0300

    Delete JavaScript-Programming-Fundamentals/03-Functions/Lab directory

--- JavaScript-Programming-Fundamentals/03-Functions/Lab/02-MathPower.js
+++
@@ -1,4 +0,0 @@
-function mathPow(n, exponent){
-    console.log(Math.pow(n, exponent));
-}
-mathPow(2,8);
--- JavaScript-Programming-Fundamentals/03-Functions/Lab/03-RepeatString.js
+++
@@ -1,9 +0,0 @@
-function repeat(str, n){
-let result = "";
-
-    for(i = 1; i <= n; i ++){
-        result += str;
-    }
-
-    console.log(result);
-}
